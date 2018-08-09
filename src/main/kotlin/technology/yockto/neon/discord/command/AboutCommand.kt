0c1add12c5ed730e8aae71adc0debdeb8496a854
danthonywalker <thedanthonywalker@gmail.com>
2018-08-09 09:32:18 -0600

    More Fine Tuning

--- src/main/kotlin/technology/yockto/neon/discord/command/AboutCommand.kt
+++ src/main/kotlin/technology/yockto/neon/discord/command/AboutCommand.kt
@@ -32,19 +32,25 @@ import java.time.Instant
 @Suppress("KDocMissingDocumentation")
 class AboutCommand : NeonCommand {
 
+    override val aliases: Set<String> = setOf("about")
+    private val startTime = Instant.now()
+
     override fun execute(event: MessageCreateEvent): Mono<Void> {
-        return Mono.zip(event.message.channel, event.client.self)
+        return Mono.zip(event.message.channel, event.client.self, event.guild)
+            .filter { getArguments(event.message.content.get()).isEmpty() }
             .flatMap { tuple ->
 
                 tuple.t1.createMessage { messageCreateSpec ->
                     messageCreateSpec.setEmbed {
 
+                        val version = VersionUtil.getProperties().getProperty(VersionUtil.APPLICATION_VERSION)
                         val os = ManagementFactory.getOperatingSystemMXBean() as OperatingSystemMXBean
                         val avatarUrl = tuple.t2.getAvatarUrl(PNG).orElse(tuple.t2.defaultAvatarUrl)
                         val authorName = "${tuple.t2.username}#${tuple.t2.discriminator}"
                         val runtime = Runtime.getRuntime()
+                        val currentTime = Instant.now()
 
-                        val uptime = Duration.ofNanos(os.processCpuTime)
+                        val uptime = Duration.between(startTime, currentTime)
                         val days = "${uptime.toDays()}d"
                         val hours = "${uptime.toHoursPart()}h"
                         val minutes = "${uptime.toMinutesPart()}m"
@@ -53,29 +59,28 @@ class AboutCommand : NeonCommand {
                         val nanos = "${uptime.toNanosPart() / 1000}ns"
 
                         val processors = "Processors: ${os.availableProcessors}"
-                        val systemCpuUsage = "System: ${(os.systemCpuLoad * 100).round(2)}%"
-                        val jvmCpuUsage = "JVM: ${(os.processCpuLoad * 100).round(2)}%"
+                        val systemCpu = "System: ${(os.systemCpuLoad * 100).round(2)}%"
+                        val jvmCpu = "JVM: ${(os.processCpuLoad * 100).round(2)}%"
 
-                        val totalSystem = os.totalPhysicalMemorySize.toDouble()
-                        val freeSystem = os.freePhysicalMemorySize.toDouble()
-                        val usedSystem = totalSystem - freeSystem
-                        val usedSystemPercent = ((usedSystem / totalSystem) * 100).round(2)
+                        val totalSystem = (os.totalPhysicalMemorySize.toDouble() / 1_000_000)
+                        val usedSystem = (totalSystem - (os.freePhysicalMemorySize.toDouble() / 1_000_000))
+                        val totalJvm = (runtime.totalMemory().toDouble() / 1_000_000)
+                        val usedJvm = (totalJvm - (runtime.freeMemory().toDouble() / 1_000_000))
 
-                        val totalJvm = runtime.totalMemory().toDouble()
-                        val freeJvm = runtime.freeMemory().toDouble()
-                        val usedJvm = totalJvm - freeJvm
-                        val usedJvmPercent = ((usedJvm / totalJvm) * 100).round(2)
+                        val percentJvmSystem = "${((usedJvm / totalSystem) * 100).round(2)}%"
+                        val percentSystem = "${((usedSystem / totalSystem) * 100).round(2)}%"
+                        val percentJvm = "${((usedJvm / totalJvm) * 100).round(2)}%"
 
-                        val totalRam = "Total RAM: ${(totalSystem / 1_000_000).round(2)}MB"
-                        val systemRam = "System: ${(usedSystem / 1_000_000).round(2)}MB ($usedSystemPercent% Used)"
-                        val jvmRam = "JVM: ${(usedJvm / 1_000_000).round(2)}MB ($usedJvmPercent% Used)"
+                        val neonRam = "Neon: ${usedJvm.round(2)}MB / ${totalSystem.round(2)}MB ($percentJvmSystem)"
+                        val systemRam = "System: ${usedSystem.round(2)}MB / ${totalSystem.round(2)}MB ($percentSystem)"
+                        val jvmRam = "JVM: ${usedJvm.round(2)}MB / ${totalJvm.round(2)}MB ($percentJvm)"
 
                         it.addField("Author", "danthonywalker#5512", true)
-                        it.addField("Powered By", "Discord4J (${VersionUtil.APPLICATION_VERSION})", true)
+                        it.addField("Powered By", "Discord4J ($version)", true)
                         it.addField("Operating System", "${os.name}-${os.arch}-${os.version}", true)
                         it.addField("Uptime", "$days $hours $minutes $seconds $millis $nanos", true)
-                        it.addField("CPU Usage", "$processors\n$systemCpuUsage\n$jvmCpuUsage", true)
-                        it.addField("RAM Usage", "$totalRam\n$systemRam\n$jvmRam", true)
+                        it.addField("CPU Usage", "$processors\n$systemCpu\n$jvmCpu", true)
+                        it.addField("RAM Usage", "$neonRam\n$systemRam\n$jvmRam", true)
 
                         it.addField("Privacy Policy", "By inviting Neon to your Guild, or by interacting with " +
                             "Features provided by Neon, then you agree to the collection and use of Information " +
@@ -85,10 +90,10 @@ class AboutCommand : NeonCommand {
                             "statistical Information such as message count and/or guild count. Users may request to " +
                             "view and/or delete their Information at any time (see Help command for details).", false)
 
-                        it.setFooter("Neon Midori Network", "http://neonmidori.net/images/theomnipresentglobe.png")
                         it.setDescription("A Discord Bot Gaming Bridge for the Neon Midori Network")
                         it.setAuthor(authorName, "https://neon.yockto.technology/", avatarUrl)
-                        it.setTimestamp(Instant.now())
+                        it.setFooter(tuple.t3.name, tuple.t3.getIconUrl(PNG).orElse(null))
+                        it.setTimestamp(currentTime)
                         it.setThumbnail(avatarUrl)
                         it.setColor(Color.CYAN)
                         it.setTitle("Summary")
@@ -96,6 +101,4 @@ class AboutCommand : NeonCommand {
                 }
             }.then()
     }
-
-    override val aliases: Set<String> = setOf("about")
 }
